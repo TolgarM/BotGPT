f8ed26066f40d1f1e60ed2d95f6f93f4e750e3bc
Tolgar <tolgorenko_m@mail.ru>
2023-03-23 22:44:43 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,130 @@
+import telebot
+import openai
+
+with open("config.ini", 'r') as config_file:
+    telegram_token = config_file.readline()
+    if telegram_token[-1] == '\n':
+        telegram_token = telegram_token[:-1]
+    openai_token = config_file.readline()
+    if openai_token[-1] == '\n':
+        openai_token = openai_token[:-1]
+    my_id = int(config_file.readline())
+
+openai.api_key = openai_token
+bot = telebot.TeleBot(telegram_token, )
+root_user_ids = [my_id]
+allowed_user_ids = [my_id]
+
+users_messages = {}
+
+default_messages_content = [{"role": "system",
+                             "content": "You are a helpful assistant that tries to answer every question."}, ]
+
+
+@bot.message_handler(commands=['add'])
+def start(message):
+
+    if message.from_user.id not in root_user_ids:
+        bot.send_message(message.from_user.id, "You don't have access to this command")
+        print(message.from_user.id, "tried to connect")
+        return
+
+    allowed_user_ids.append(int(message.text.split(' ')[-1]))
+    print(allowed_user_ids)
+    bot.send_message(message.from_user.id, f"Successfully added new user id {allowed_user_ids[-1]}")
+
+
+@bot.message_handler(commands=['remove'])
+def start(message):
+
+    if message.from_user.id not in root_user_ids:
+        bot.send_message(message.from_user.id, "You don't have access to this command")
+        print(message.from_user.id, "tried to connect")
+        return
+
+    try:
+        allowed_user_ids.remove(int(message.text.split(' ')[-1]))
+        print(allowed_user_ids)
+        bot.send_message(message.from_user.id, f"Successfully removed user id {message.text.split(' ')[-1]}")
+    except Exception as e:
+        bot.send_message(message.from_user.id, f"Error removing user id: {e}")
+
+
+@bot.message_handler(commands=['ls'])
+def start(message):
+
+    if message.from_user.id not in root_user_ids:
+        bot.send_message(message.from_user.id, "You don't have access to this command")
+        print(message.from_user.id, "tried to connect")
+        return
+
+    bot.send_message(message.from_user.id, f"{allowed_user_ids}")
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+
+    if message.from_user.id not in allowed_user_ids:
+        bot.send_message(message.from_user.id, "You don't have access to this bot")
+        print(message.from_user.id, "tried to connect")
+        return
+
+    bot.send_message(message.from_user.id, "Hi! This bot is capable of using ChatGPT model")
+
+
+@bot.message_handler(commands=['clear'])
+def clear(message):
+
+    if message.from_user.id not in allowed_user_ids:
+        bot.send_message(message.from_user.id, "You don't have access to this bot")
+        print(message.from_user.id, "tried to connect")
+        return
+
+    if message.from_user.id in users_messages:
+        users_messages[message.from_user.id] = []
+
+    print(users_messages)
+
+    bot.send_message(message.from_user.id, "Cleared conversation")
+
+
+@bot.message_handler(content_types=['text'])
+def process_text_messages(message):
+
+    if message.from_user.id not in allowed_user_ids:
+        bot.send_message(message.from_user.id, "You don't have access to this bot")
+        print(message.from_user.id, "tried to connect")
+        return
+
+    if message.from_user.id not in users_messages:
+        users_messages[message.from_user.id] = default_messages_content.copy()
+
+    users_messages[message.from_user.id].append({"role": "user", "content": message.text})
+    previous_messages = users_messages[message.from_user.id]
+
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=previous_messages
+        )
+    except openai.error.APIError as e:
+        bot.send_message(message.from_user.id, f"APIError:{e}")
+    except Exception as e:
+        bot.send_message(message.from_user.id, f"Error:{e}")
+        return
+
+    users_messages[message.from_user.id].append({"role": "assistant",
+                                                 "content": response["choices"][0]["message"]["content"]})
+
+    print(users_messages)
+
+    bot.send_message(message.from_user.id, response["choices"][0]["message"]["content"])
+
+
+while True:
+    try:
+        bot.polling(none_stop=True, interval=0)
+    except KeyboardInterrupt:
+        exit()
+    except Exception as e:
+        print(e)
